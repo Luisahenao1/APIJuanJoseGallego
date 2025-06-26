956ae07728c193ec39119460eb3274ff1e27f122
cely <kevinlaverde3@gmail.com>
2025-06-26 14:30:35 -0500

    se prueba el api

--- SpringBootJuanfe13/src/main/java/com/example/SpringBootJuanfe13/SpringBootJuanfe13Application.java
+++ SpringBootJuanfe13/src/main/java/com/example/SpringBootJuanfe13/SpringBootJuanfe13Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootJuanfe13Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJuanfe13Application.class, args);
-	}
+		SpringApplication.run(SpringBootJuanfe13Application.class, args);}
 
 }
--- SpringBootJuanfe13/src/main/java/com/example/SpringBootJuanfe13/controladores/UsuarioControlador.java
+++ SpringBootJuanfe13/src/main/java/com/example/SpringBootJuanfe13/controladores/UsuarioControlador.java
@@ -5,6 +5,7 @@ import com.example.SpringBootJuanfe13.servcios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,8 @@ public class UsuarioControlador {
 
     //FUNCION para activar el servicio de
     //guardado
+
+    @PostMapping
 
     public ResponseEntity<?> activarGuardado(@RequestBody Usuario datosEnviados){
         try{
